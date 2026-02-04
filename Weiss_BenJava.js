e51266dbf31e7438cd8d53ddf9d9886d8b836c4d
BenWeiss45 <29benjamin.weiss@boiseschools.net>
2026-02-04 09:08:17 -0700

    Add functions to prompt user for preferences

---
+++ Weiss_BenJava.js
@@ -0,0 +1,24 @@
+function first(){
+var icecream = prompt("What is your favorite icecream flavor?");
+if (icecream != "") {
+alert("Your favorite flavor is " + icecream + " ice cream!");
+} else {
+    alert("You dont have a favorite ice cream flavor");
+}
+}
+function secound(){
+var name = prompt("What is your name?");
+if (name != "") {
+alert("Your name is " + name + "!");
+} else {
+    alert("You dont have a name, weird O_o");
+}
+}
+function third(){
+var car = prompt("What is your favorite car?");
+if (car != "") {
+alert("Your favorite car is " + car + "!");
+} else {
+    alert("You dont have a favorite car, go to the image gallery to find mine ^_^");
+}
+}
